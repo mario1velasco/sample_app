c355604d11c27c39c9f29ff6a7c30f597117f0df
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-06-07 20:33:17 +0200

    Add full_title help and test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,10 +5,14 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | Home")
+      :text => "Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
   describe "Help page" do
@@ -16,10 +20,14 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
-    it "should have the title 'Help'" do
+    it "should have the base title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | Help")
+      :text => "Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title', :text => '| Help')
     end
   end
   describe "About page" do
@@ -27,10 +35,14 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end
-    it "should have the title 'About Us'" do
+    it "should have the base title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | About Us")
+      :text => "Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/about'
+      page.should_not have_selector('title', :text => '| About')
     end
   end
 end
